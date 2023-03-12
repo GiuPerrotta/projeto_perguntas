0a8cde230bd0f5fd8b18d453da080de18c5d76c4
Giuliano Perrotta <giuperrotta@gmail.com>
2023-03-11 21:09:00 -0300

    exercicio

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const PerguntaApp());
+  runApp(PerguntaApp());
 }
 
 class PerguntaApp extends StatelessWidget {
-  const PerguntaApp({super.key});
+  PerguntaApp({super.key});
+
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    perguntaSelecionada++;
+    print(perguntaSelecionada);
+  }
 
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
-        body: const Text('Olá Flutter!!!'),
+        body: Column(
+          children: [
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
